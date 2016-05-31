ee0b4dec3a989f15d36ac60a4c92feddd8971d7a
Stuart Smith <ssmith_1975@yahoo.com>
2016-05-30 17:37:23 -0700

    Forgot to add INotify proeperty change to the base view model

--- ControlSg90Example/ViewModels/BaseViewModel/ViewModelBase.cs
+++ ControlSg90Example/ViewModels/BaseViewModel/ViewModelBase.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 
 namespace ControlSg90Example.ViewModels.BaseViewModel
 {
-    public class ViewModelBase
+    public class ViewModelBase:INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged = delegate { }; 
 
